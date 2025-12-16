c5e0ea2614df731a9f7e19883b0d941c9444963c
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-12-16 19:27:54 +0100

    Add critters for improved performance and update browserslist for compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ["lucide-react", "framer-motion"],
+  },
 };
 
 export default nextConfig;
